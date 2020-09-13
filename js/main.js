13be499ec1cff0c016cbcd0d25d902da304ea308
DimaSilver <68532590+DimaSilver@users.noreply.github.com>
2020-09-13 07:22:58 +0300

    Застилизовал секшэн types и исправил недочёты

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(function(){
-	
+    $('.types__content-slider').slick({
+        prevArrow:'<button type="button" class="types__slider-arrows types__slider-prev"></button>',
+        nextArrow:'<button type="button" class="types__slider-arrows types__slider-next"></button>',
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        infinite: false
+    });
+    $('.tab').on('click', function (e) {
+       e.preventDefault();
+       $($(this).siblings()).removeClass('tab--active');
+       $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
+       $(this).addClass('tab--active');
+       $($(this).attr('href')).addClass('tabs-content--active');
+       $('.types__content-slider').slick('setPosition');
+    });
+
 });
