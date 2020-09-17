f149b5bd4c7d28aa8a829b27f6a70ae8f03e7b6d
DimaSilver <68532590+DimaSilver@users.noreply.github.com>
2020-09-17 19:14:36 +0300

    Адаптив

--- js/main.js
+++ js/main.js
@@ -4,7 +4,49 @@ $(function(){
         nextArrow:'<button type="button" class="types__slider-arrows types__slider-next"></button>',
         slidesToShow: 7,
         slidesToScroll: 1,
-        infinite: false
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 1651,
+                settings: {
+                    slidesToShow: 6,
+                }
+            },
+            {
+                breakpoint: 1601,
+                settings: {
+                    slidesToShow: 5,
+                }
+            },
+            {
+                breakpoint: 1216,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 731,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                    dots: true,
+                    dotsClass: 'types__content-slider__dots'
+                }
+            },
+            {
+                breakpoint: 486,
+                settings: {
+                    destroy: true,
+                    arrows: false
+                }
+            }
+        ]
     });
     $('.tab').on('click', function (e) {
        e.preventDefault();
@@ -16,5 +58,9 @@ $(function(){
     });
     $('.footer__list').on('click', '.footer__list-item',  function(){
         $(this).addClass('footer__list-item--active').siblings().removeClass('footer__list-item--active')
+    });
+    $('.menu__btn').on('click', function(){
+        $(this).toggleClass('menu__btn--active');
+        $('.menu__mobile').toggleClass('menu__mobile--active');
     });
 });
